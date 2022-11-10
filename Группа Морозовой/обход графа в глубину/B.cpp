6a655df3967a8b88862f51b0024382c506f3c650
qrivthv <117040407+qrivthv@users.noreply.github.com>
2022-11-10 22:02:11 +0300

    B

---
+++ Группа Морозовой/обход графа в глубину/B.cpp
@@ -0,0 +1,31 @@
+﻿#include <vector>
+#include <iostream>
+using namespace std;
+bool* visited;
+vector <int>* a;
+int dfs(int p) {
+	visited[p] = true;
+	int counter = 1;
+	for (auto i : a[p]) {
+		if (!visited[i]) {
+			counter += dfs(i);
+		}
+	}
+	return counter;
+}
+int main() {
+	int n, p, k; cin >> n >> p;
+	a = new vector<int>[n];
+	visited = new bool[n];
+	p--;
+	for (int i = 0; i < n; i++) visited[i] = false;
+	for (int i = 0; i < n; i++)
+		for (int j = 0; j < n; j++) {
+			cin >> k;
+			if (k == 1) {
+				a[i].push_back(j);
+			}
+		}
+	cout << dfs(p);
+	return 0;
+}
